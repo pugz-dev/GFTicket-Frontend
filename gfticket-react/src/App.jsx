ca1ff499b2efb268a5e4616efef05fe321af8c13
JEIJ <jeij@gft.com>
2026-07-14 11:15:33 +0200

    feat: Initial routes & PageNotFound

--- gfticket-react/src/App.jsx
+++ gfticket-react/src/App.jsx
@@ -1,5 +1,26 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link, Outlet, Navigate } from "react-router-dom";
+import { EventList } from './components/event-list/EventList';
+import { PageNotFound } from './components/page-not-found/PageNotFound'; 
 import './App.css'
+
+function Layout(){
+  return(
+    <>
+    <div><h1>GFTICKET - ADMIN PANEL</h1></div>
+    <nav>
+      <Link to="/eventos" className="btn btn-link">
+        List
+      </Link>
+      <Link to="/eventos/add" className="btn btn-link" style={{ marginLeft: "10px" }}>
+        Add
+      </Link>
+    </nav>
+    <br />
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +28,23 @@ function App() {
   return (
     <>
     Gfticket works
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+         <Route index element={<Navigate to="/eventos" replace />} />
+
+          <Route path="eventos" element={<EventList />} />
+          {/*
+          <Route path="eventos/:id" element={<EventDetail />} />
+          <Route path="eventos/nuevo" element={<EventAdd />} />
+          <Route path="eventos/edit/:id" element={<EventEdit />} /> 
+           */}
+        </Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
+    
   )
 }
 
