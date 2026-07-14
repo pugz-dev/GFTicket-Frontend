2a7238f579ce4741418fc55571e1379e4532257f
paugreus-jpg <pau.greus@gmail.com>
2026-07-14 12:17:30 +0200

    feat(SCRUM-79): configure folder structure and routes

--- gfticket-angular/src/app/app.routes.ts
+++ gfticket-angular/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'events',
+        loadComponent: () => import('./pages/event-list/event-list').then(m => m.EventList)
+    },
+
+    {
+        path: 'events/:id',
+        loadComponent: () => import('./pages/event-detail/event-detail').then(m => m.EventDetail)
+    },
+
+    {
+        path: '',
+        redirectTo: 'events',
+        pathMatch: 'full'
+    }
+];
+
+
