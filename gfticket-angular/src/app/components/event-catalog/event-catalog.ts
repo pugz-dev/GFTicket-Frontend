19ebb030d17aed0e144fcce2a7d424a53bfc7f83
paugreus-jpg <pau.greus@gmail.com>
2026-07-15 11:03:45 +0200

    test(SCRUM-84): add EventCatalog component tests before implementation

---
+++ gfticket-angular/src/app/components/event-catalog/event-catalog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-catalog',
+  imports: [],
+  templateUrl: './event-catalog.html',
+  styleUrl: './event-catalog.css',
+})
+export class EventCatalog {}
