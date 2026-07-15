98d0a518182c07ffa87440edede97e49e6a0b6bd
JEIJ <jeij@gft.com>
2026-07-15 14:29:36 +0200

    feat: eventList components with fetch result and loading messages

--- gfticket-react/src/components/event-list/EventList.jsx
+++ gfticket-react/src/components/event-list/EventList.jsx
@@ -8,6 +8,7 @@ export function EventList() {
 
     const [events, setEvents] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     const fetchEvents = async () => {
         try {
@@ -15,52 +16,59 @@ export function EventList() {
           setEvents(data);
         } catch (err) {
           console.error(err);
-          alert('Error loading events.');
+          setError(true);
         } finally {
-          setLoading(false); 
+          setLoading(false);
         }
-      };
+    };
 
     useEffect(() => {
 
         const timer = setTimeout(() => {
-            fetchLibros();
+            fetchEvents();
         }, 0);
 
         return () => clearTimeout(timer);
 
     }, []);
-    
+
     return (
         <div>
-            <section className="ListaLibros">
-            <h2>Listado de Libros</h2>
-            <p>Aquí se mostrarán los libros disponibles en la biblioteca.</p>
+            <section className="ListaEventos">
+            <h2>Listado de Eventos</h2>
+            <p>Aquí se mostrarán los eventos disponibles.</p>
             </section>
-            <input type='text' name='filtroTitulo' placeholder='Filter by title'></input>
-            {libros.length === 0 && <p>No hay libros disponibles.</p>}
-            {libros.length > 0 && (
+            {loading && <p>Cargando eventos...</p>}
+            {!loading && error && <div className="alert alert-danger" role="alert">Error al cargar los eventos.</div>}
+            {events.length === 0 && !loading && !error && <p>No hay eventos disponibles.</p>}
+            {events.length > 0 && (
                 <div>
-                    <span> Hay {libros.length} libros disponibles</span>
+                    <span> Hay {events.length} eventos disponibles</span>
                     <table>
                         <thead>
                             <tr>
-                                <th>Título</th>
-                                <th>Autor</th>
-                                <th>Páginas</th>
+                                <th>Nombre</th>
+                                <th>Fecha</th>
+                                <th>Hora</th>
+                                <th>Rango Precio</th>
+                                <th>Localidad</th>
                                 <th>Género</th>
+                                <th>Recinto</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {libros.map((libro) => (
-                                <tr key={libro.id}>
-                                    <td><Link to={`/libros/${libro.id}`}>{libro.titulo}</Link></td>
-                                    <td>{libro.autor}</td>
-                                    <td>{libro.paginas}</td>
-                                    <td>{darGeneroLibro(libro.genero)}</td>
+                            {events.map((evento) => (
+                                <tr key={evento.id}>
+                                    <td><Link to={`/eventos/${evento.id}`}>{evento.nombre}</Link></td>
+                                    <td>{evento.fechaEvento}</td>
+                                    <td>{evento.horaEvento}</td>
+                                    <td>{evento.precioMinimo ?? 'N/A'}€ ~ {evento.precioMaximo ?? 'N/A'}€</td>
+                                    <td>{evento.localidad}</td>
+                                    <td>{evento.genero}</td>
+                                    <td>{evento.nombreRecinto}</td>
                                     {/*
                                     <td>
-                                        <button onClick={() => handleDelete(libro.id)}>🗑️</button>
+                                        <button onClick={() => handleDelete(evento.id)}>🗑️</button>
                                     </td>
                                     */}
                                 </tr>
@@ -69,9 +77,6 @@ export function EventList() {
                     </table>
                 </div>
             )}
-            {/*
-            <AltaLibro currentLength={libros.length} onAddLibro={onAddLibro} />
-            */}
         </div>
     );
 }
