662e9b23aa9978ed6c46f01ebf155dbf4e34f56e
JEIJ <jeij@gft.com>
2026-07-15 16:50:45 +0200

    feat: eventDetail css style implementation

--- gfticket-react/src/components/event-detail-component/EventDetailComponent.jsx
+++ gfticket-react/src/components/event-detail-component/EventDetailComponent.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getEventById } from '../../services/eventService';
-//import './EventDetailComponent.css';
+import './EventDetailComponent.css';
 
 export function EventDetail() {
     const { id } = useParams();
