08258282584d8c98f22a94f021f80a25bdb62093
paugreus-jpg <pau.greus@gmail.com>
2026-07-15 13:17:16 +0200

    feat(SCRUM-87): navigate from EventCatalog cards to event detail

--- gfticket-angular/src/app/components/event-catalog/event-catalog.ts
+++ gfticket-angular/src/app/components/event-catalog/event-catalog.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { EventModel } from "../../models/event.model";
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-event-catalog',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './event-catalog.html',
   styleUrl: './event-catalog.css',
 })
