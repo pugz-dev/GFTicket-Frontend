e3cde94d55dcb0297c04095d79058d9d55517b31
pugz-dev <pugz@gft.com>
2026-07-21 15:22:37 +0200

    feat(SCRUM-292): confirmation purchase logic and structure

--- gfticket-angular/src/app/pages/confirmation-purchase/confirmation-purchase.ts
+++ gfticket-angular/src/app/pages/confirmation-purchase/confirmation-purchase.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { PurchaseService } from '../../services/purchase.service';
 
 @Component({
   selector: 'app-confirmation-purchase',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './confirmation-purchase.html',
   styleUrl: './confirmation-purchase.css',
 })
-export class ConfirmationPurchase {}
+export class ConfirmationPurchase {
+  private readonly purchaseService = inject(PurchaseService);
+
+  success: boolean = true;
+  mensaje: string = '';
+  eventId?: number;
+
+  constructor() {
+    const state = history.state as { success: boolean; codigoError: string; eventId?: number };
+    this.success = state.success;
+    this.eventId = state.eventId;
+    this.mensaje = this.success ? 'Tu compra se ha realizado correctamente' : this.purchaseService.getMensajeError(state.codigoError);
+  }
+}
